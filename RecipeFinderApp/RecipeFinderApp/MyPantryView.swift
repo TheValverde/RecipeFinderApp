854010e12f207fb5354fe2ba55379195eb466642
Hugo Valverde <hvalverde@quinceimaging.com>
2023-06-09 02:37:51 -0400

    Got rid of the CoreData for now

--- RecipeFinderApp/RecipeFinderApp/MyPantryView.swift
+++ RecipeFinderApp/RecipeFinderApp/MyPantryView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct MyPantryView: View {
+    @EnvironmentObject var viewModel: RecipeViewModel
+
     var body: some View {
-        Text("My Pantry Screen")
+        VStack {
+            Text("My Pantry")
+                .font(.title)
+                .padding()
+            
+            List(viewModel.userIngredients) { ingredient in
+                HStack {
+                    Text(ingredient.name)
+                    Spacer()
+                    Text("\(ingredient.quantity, specifier: "%.2f") \(ingredient.unit)")
+                }
+            }
+        }
     }
 }
+
 
 
 struct MyPantryView_Previews: PreviewProvider {
     static var previews: some View {
         MyPantryView()
+            .environmentObject(RecipeViewModel())
     }
 }
+
